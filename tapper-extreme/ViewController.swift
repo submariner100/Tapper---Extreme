9eaee2ae7cb7da5b65d82ab963ed78868f962dfe
Michael Chapman <submariner100@gmail.com>
2015-10-16 18:13:31 +0100

    Game completed

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  
+  
+  //Properties
+  var maxTaps:Int = 0
+  var currentTaps:Int = 0
+  
+  // Outlets
+  @IBOutlet weak var logoImage: UIImageView!
+  @IBOutlet weak var howManyTapsTxt: UITextField!
+  @IBOutlet weak var playBtn: UIButton!
+  
+  @IBOutlet weak var tapBtn: UIButton!
+  @IBOutlet weak var tapsLbl: UILabel!
+  
+  @IBAction func onCoinTapped(sender: UIButton!) {
+      currentTaps++
+      updateTapsLabel()
+    if isGameOver() {
+      
+      restartGame()
+      
+    }
+   }
+  
+  @IBAction func onPlayBtnPressed(sender: UIButton!) {
+    
+    if howManyTapsTxt.text != nil  && howManyTapsTxt.text != "" {
+     
+        logoImage.hidden = true
+        playBtn.hidden = true
+        howManyTapsTxt.hidden = true
+      
+        tapBtn.hidden = false
+        tapsLbl.hidden = false
+      
+      
+        maxTaps = Int(howManyTapsTxt.text!)!
+        currentTaps = 0
+        updateTapsLabel()
+      
+      }
+   }
+  
+  func restartGame() {
+    maxTaps = 0
+    howManyTapsTxt.text = ""
+    logoImage.hidden = false
+    playBtn.hidden = false
+    howManyTapsTxt.hidden = false
+    
+    tapBtn.hidden = true
+    tapsLbl.hidden = true
+    
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  
+  func isGameOver() -> Bool {
+    if currentTaps >= maxTaps {
+        return true
+    } else {
+        return false
+      }
   }
-
-
+  
+  func updateTapsLabel() {
+    tapsLbl.text = "\(currentTaps) Taps"
+    
+    
+  }
+  
 }
 
